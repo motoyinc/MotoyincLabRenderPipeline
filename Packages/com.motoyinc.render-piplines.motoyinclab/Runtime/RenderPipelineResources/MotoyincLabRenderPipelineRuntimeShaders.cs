27669d76dd72e21c6447954f6b939a26d1154bef
motoyinc <motoyinc@gmail.com>
2024-07-29 22:49:29 +0800

    Add 新增资源以及必要Shader文件

---
+++ Packages/com.motoyinc.render-piplines.motoyinclab/Runtime/RenderPipelineResources/MotoyincLabRenderPipelineRuntimeShaders.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace UnityEngine.Rendering.MotoyincLab
+{
+    public class MotoyincLabRenderPipelineRuntimeShaders : IRenderPipelineResources
+    {
+        // 资源版本
+        [SerializeField][HideInInspector] private int m_Version = 0;
+        public int version => m_Version;
+        
+        // 管线Shader资源
+        bool IRenderPipelineGraphicsSettings.isAvailableInPlayerBuild => true;
+        [SerializeField]
+        [ResourcePath("Shaders/Utils/CoreBlit.shader")]
+        internal Shader m_CoreBlitPS;
+        
+        public Shader coreBlitPS
+        {
+            get => m_CoreBlitPS;
+            set => this.SetValueAndNotify(ref m_CoreBlitPS, value, nameof(m_CoreBlitPS));
+        }
+
+        [SerializeField]
+        [ResourcePath("Shaders/Utils/CoreBlitColorAndDepth.shader")]
+        internal Shader m_CoreBlitColorAndDepthPS;
+        
+        public Shader coreBlitColorAndDepthPS
+        {
+            get => m_CoreBlitColorAndDepthPS;
+            set => this.SetValueAndNotify(ref m_CoreBlitColorAndDepthPS, value, nameof(m_CoreBlitColorAndDepthPS));
+        }
+    }
+}
---
+++ Packages/com.motoyinc.render-piplines.motoyinclab/Runtime/RenderPipelineResources/MotoyincLabRenderPipelineRuntimeTextures.cs
@@ -0,0 +1,51 @@
+using System;
+using NUnit.Framework.Constraints;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
+
+namespace UnityEngine.Rendering.MotoyincLab
+{
+    [Serializable]
+    [SupportedOnRenderPipeline(typeof(MotoyincLabRenderPipelineAsset))]
+    public class MotoyincLabRenderPipelineRuntimeTextures : IRenderPipelineResources
+    {
+        // 资源版本
+        [SerializeField][HideInInspector] private int m_Version = 0;
+        public int version => m_Version;
+        
+        bool IRenderPipelineGraphicsSettings.isAvailableInPlayerBuild => true;
+
+        // 管线Texture资源
+        [SerializeField]
+        [ResourcePath("Textures/BlueNoise64/L/LDR_LLL1_0.png")]
+        private Texture2D m_BlueNoise64LTex;
+
+        public Texture2D blueNoise64LTex
+        {
+            get => m_BlueNoise64LTex;
+            set => this.SetValueAndNotify(ref m_BlueNoise64LTex, value, nameof(m_BlueNoise64LTex));
+        }
+        
+        [SerializeField]
+        [ResourcePath("Textures/BayerMatrix.png")]
+        private Texture2D m_BayerMatrixTex;
+        
+        public Texture2D bayerMatrixTex
+        {
+            get => m_BayerMatrixTex;
+            set => this.SetValueAndNotify(ref m_BayerMatrixTex, value, nameof(m_BayerMatrixTex));
+        }
+
+        [SerializeField]
+        [ResourcePath("Textures/DebugFont.tga")]
+        private Texture2D m_DebugFontTex;
+        
+        public Texture2D debugFontTexture
+        {
+            get => m_DebugFontTex;
+            set => this.SetValueAndNotify(ref m_DebugFontTex, value, nameof(m_DebugFontTex));
+        }
+
+
+
+    }
+}
