0b70ec4cb25ca25069b6b09d74a7cbbd0740a91a
motoyinc <motoyinc@gmail.com>
2024-07-31 07:47:47 +0800

    #4 [Featrue] Pipeline 的 Render() 方法的摄像机循环框架
    ## **Pipeline.Render**
    - Render( ) 添加摄像机循环，以及调用摄像机渲染的方法
    
    ## **Pipeline.RenderCamera**
    - RenderSingleCameraInternal( ) 通过MotoyincLabAdditionalCameraData创建渲染器Renderer
    - RenderCameraStack( ) 通过MotoyincLabAdditionalCameraData创建渲染器Renderer
    
    - RenderSingleCamera( ) 渲染相机 Render到最后执行的方法
    
    ## **MotoyincLabAdditionalCameraData**
    -  这是一个摄像机Compomet组件，用来挂载帧数据
    
    - scriptableRenderer 公开属性 用来获取渲染器Renderer

--- Packages/com.motoyinc.render-piplines.motoyinclab/Runtime/RenderPipeline/MotoyincLabRenderPipeline.Render.cs
+++ Packages/com.motoyinc.render-piplines.motoyinclab/Runtime/RenderPipeline/MotoyincLabRenderPipeline.Render.cs
@@ -1,19 +1,88 @@
-﻿namespace UnityEngine.Rendering.MotoyincLab
+﻿using System;
+using Unity.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine.Rendering.MotoyincLab;
+#endif
+using UnityEngine.Scripting.APIUpdating;
+using Lightmapping = UnityEngine.Experimental.GlobalIllumination.Lightmapping;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Profiling;
+using static UnityEngine.Camera;
+
+namespace UnityEngine.Rendering.MotoyincLab
 {
     public partial class MotoyincLabRenderPipeline
     {
         protected override void Render(ScriptableRenderContext context, Camera[] cameras)
         {
+            Render(context, new List<Camera>(cameras));
+        }
+        protected override void Render(ScriptableRenderContext context, List<Camera> cameras)
+        {
+            int cameraCount = cameras.Count;
+            
+            // 摄像机循环
+            using var profScope = new ProfilingScope(ProfilingSampler.Get(MLRPProfileId.MotoyincLabRenderTotal));
+            using (new ContextRenderingScope(context, cameras))
+            {
+                for (int i = 0; i < cameras.Count; ++i)
+                {
+                    var camera = cameras[i];
+                    RTHandles.SetHardwareDynamicResolutionState(true);
+                    
+                    // 判断摄像机是否存在 如果不存在直接抛出异常
+                    if (camera == null)
+                        throw new ArgumentNullException("camera");
+
+                    if (isGameCamera(camera))
+                    {
+                        RenderCameraStack(context, camera);
+                    }
+                    else
+                    {
+                        RenderSingleCameraInternal(context,camera);
+                    }
+                }
+            }
             
         }
 
-        static void RenderSingleCamera(ScriptableRenderContext context, MotoyincLabCameraData cameraData)
+        public static bool isGameCamera(Camera camera)
         {
+            return camera.cameraType == CameraType.Game || camera.cameraType == CameraType.VR;
         }
 
-        static void RenderCameraStack(ScriptableRenderContext context, Camera baseCamera)
+        // 性能分析用结构体
+        readonly struct ContextRenderingScope : IDisposable
         {
+            private static readonly ProfilingSampler beginContextRenderingSampler = new ProfilingSampler($"{nameof(RenderPipeline)}.{nameof(BeginContextRendering)}");
+            private static readonly ProfilingSampler endContextRenderingSampler = new ProfilingSampler($"{nameof(RenderPipeline)}.{nameof(EndContextRendering)}");
+            private readonly ScriptableRenderContext m_Context;
+            private readonly List<Camera> m_Cameras;
+            
+            public ContextRenderingScope(ScriptableRenderContext context, List<Camera> cameras)
+            {
+                m_Context = context;
+                m_Cameras = cameras;
+                using (new ProfilingScope(beginContextRenderingSampler))
+                {
+                    BeginContextRendering(m_Context, m_Cameras);
+                }
+            }
+            
+            public void Dispose()
+            {
+                using (new ProfilingScope(endContextRenderingSampler))
+                {
+                    EndContextRendering(m_Context, m_Cameras);
+                }
+            }
         }
+
         
+
     }
 }
---
+++ Packages/com.motoyinc.render-piplines.motoyinclab/Runtime/RenderPipeline/MotoyincLabRenderPipeline.RenderCamera.cs
@@ -0,0 +1,55 @@
+﻿
+namespace UnityEngine.Rendering.MotoyincLab
+{
+    public partial class MotoyincLabRenderPipeline
+    {
+        // 兼容性相机的 RenderCamera
+        public static void RenderSingleCamera(ScriptableRenderContext context,Camera camera)
+        {
+            RenderSingleCameraInternal(context,camera);
+        }
+        
+        // 其他类型相机的 RenderCamera
+        internal static void RenderSingleCameraInternal(ScriptableRenderContext context, Camera camera)
+        {
+            MotoyincLabAdditionalCameraData additionalCameraData = null;
+            if (isGameCamera(camera))
+                camera.gameObject.TryGetComponent(out additionalCameraData);
+
+            RenderSingleCameraInternal(context,camera,ref additionalCameraData);
+        }
+        
+        internal static void RenderSingleCameraInternal(ScriptableRenderContext context, Camera camera, ref MotoyincLabAdditionalCameraData additionalCameraData)
+        {
+            // 获取 Renderer
+            camera.TryGetComponent<MotoyincLabAdditionalCameraData>(out var baseAdditionalCameraData);
+            ScriptableRenderer renderer = null;
+            if (baseAdditionalCameraData != null)
+                renderer = baseAdditionalCameraData.scriptableRenderer;
+            if (renderer == null || camera.cameraType == CameraType.SceneView)
+                renderer = asset.scriptableRenderer;
+            
+        }
+        
+        
+        // 正常游戏相机的 RenderCamera
+        static void RenderCameraStack(ScriptableRenderContext context, Camera baseCamera)
+        {
+            // 获取 Renderer
+            baseCamera.TryGetComponent<MotoyincLabAdditionalCameraData>(out var baseAdditionalCameraData);
+            ScriptableRenderer renderer = null;
+            if (baseAdditionalCameraData != null)
+                renderer = baseAdditionalCameraData.scriptableRenderer;
+            if (renderer == null || baseCamera.cameraType == CameraType.SceneView)
+                renderer = asset.scriptableRenderer;
+        }
+        
+        
+        // RenderCamera
+        static void RenderSingleCamera(ScriptableRenderContext context, MotoyincLabCameraData cameraData)
+        {
+            
+        }
+        
+    }
+}
