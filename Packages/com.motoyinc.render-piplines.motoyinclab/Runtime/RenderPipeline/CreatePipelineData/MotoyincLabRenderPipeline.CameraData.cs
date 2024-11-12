3c8681870e8e5590d2e1ca7d870f72dce27ad780
motoyinc <motoyinc@gmail.com>
2024-11-12 16:25:23 +0800

    #20 [Refactor] 整理RenderPipelien类中 创建各类管线数据的方法
    主要是将各种创建整理 FrameData 的方法单独放在文件里，放在一起太乱了也不好找

---
+++ Packages/com.motoyinc.render-piplines.motoyinclab/Runtime/RenderPipeline/CreatePipelineData/MotoyincLabRenderPipeline.CameraData.cs
@@ -0,0 +1,46 @@
+﻿namespace UnityEngine.Rendering.MotoyincLab
+{
+    public partial class MotoyincLabRenderPipeline
+    {
+        static MotoyincLabCameraData CreateCameraData(ContextContainer frameData, Camera camera, MotoyincLabAdditionalCameraData additionalCameraData, bool resolveFinalTarget)
+        {
+            var renderer = GetRenderer(camera, additionalCameraData);
+            MotoyincLabCameraData cameraData = frameData.Create<MotoyincLabCameraData>();
+            cameraData.Reset();
+            cameraData.camera = camera;
+            
+            // 初始化填充CameraData
+            InitializeStackedCamerData(camera, additionalCameraData, resolveFinalTarget, cameraData);
+            
+            // ...
+            
+            return cameraData;
+        }
+        
+        // 初始化 CameraData 通用部分数据
+        static void InitializeStackedCamerData(Camera baseCamera, MotoyincLabAdditionalCameraData additionalCameraData,
+            bool resolveFinalTarget, MotoyincLabCameraData cameraData)
+        {
+            var settings = asset;
+
+            // 填充数据
+            cameraData.cameraType = baseCamera.cameraType;
+            // ... 
+        }
+        
+        // 初始化 CameraData 专用部分数据
+        static void InitializeAdditionalCameraData(Camera camera, MotoyincLabAdditionalCameraData additionalCameraData,
+            bool resolveFinalTarget, MotoyincLabCameraData cameraData)
+        {
+            var renderer = GetRenderer(camera, additionalCameraData);
+            var settings = asset;
+            
+            // 相机坐标
+            cameraData.worldSpaceCameraPos = camera.transform.position;
+            
+            // 填充数据
+            cameraData.renderer = renderer;
+            // ... 
+        }
+    }
+}
