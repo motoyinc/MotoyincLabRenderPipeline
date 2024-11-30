fa0012620c3b1461e6da99b5d1aa572d4b849a0c
motoyinc <motoyinc@gmail.com>
2024-12-01 02:47:51 +0800

    #36 [Refactor] 将Shader中的将 GetDirectionalLight() 换成 GetMainLight()

--- Packages/com.motoyinc.render-piplines.motoyinclab/ShaderLibrary/Lighting.hlsl
+++ Packages/com.motoyinc.render-piplines.motoyinclab/ShaderLibrary/Lighting.hlsl
@@ -5,8 +5,16 @@
 //      仅用于编辑时不报错，防止嵌套式依赖关系，使代码变得难以维护
 //      但也意味着，使用该库，必须包含以下依赖库
 #include "SurfaceData.hlsl"
-#include "Light.hlsl"
+#include "Shadows.hlsl"
+#include "RealtimeLight.hlsl"
 #include "BRDFData.hlsl"
+
+// MainLightShadowData GetMainLightShadowData () {
+//     MainLightShadowData data;
+//     data.strength = _MainLightShadowData.x;
+//     // data.tileIndex = _DirectionalLightShadowData.y;
+//     return data;
+// }
 
 float3 LightingLambert(SurfaceData surface, Light light) {
     float3 diffuse = dot(surface.normal, light.direction);
@@ -24,7 +32,7 @@ float3 GetLighting(SurfaceData surface, BRDFData brdf, Light light)
 float3 GetLighting (SurfaceData surface, BRDFData brdf, float3 positionWS) {
     float3 color = 0.0;
     // 计算直射光
-    color = GetLighting(surface, brdf, GetDirectionalLight());
+    color = GetLighting(surface, brdf, GetMainLight());
     
     // 累计计算附加光
     for(int i = 0 ; i< GetAdditionalLightCount(); ++i)
